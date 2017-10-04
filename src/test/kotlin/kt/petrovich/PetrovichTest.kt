d2e93840965b8e8b037507ab36295260d1837526
Dmitrii Kniazev <mylog00@ya.ru>
2017-10-04 22:34:23 +0300

    #11 Upgrade to Junit5;
    - updated Junit:[4.12->5.0.1]
    - fixed tests and test resources
    - added Junit Gradle plugin [1.0.1]

--- src/test/kotlin/kt/petrovich/PetrovichTest.kt
+++ src/test/kotlin/kt/petrovich/PetrovichTest.kt
@@ -1,9 +1,10 @@
 package kt.petrovich
 
-import org.junit.Test
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import kotlin.test.assertTrue
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvFileSource
 
 
 /**
@@ -11,100 +12,55 @@ import kotlin.test.assertTrue
  * @since 16.08.2017
  */
 class PetrovichTest {
+    companion object {
+        private val PETROVICH: Petrovich = Petrovich()
+    }
+    
+    @DisplayName("Gender detection test")
     @Test
     fun detectGenderTest() {
-        val maleString = "Алексеевич"
-        val femaleString = "Алексеевна"
-        val wrongString = "блаблабла"
+        val maleGender = Gender.detectGender("Алексеевич")
+        val femaleGender = Gender.detectGender("Алексеевна")
+        val androgynousGender = Gender.detectGender("блаблабла")
 
-        assertTrue { Gender.detectGender(maleString) == Gender.MALE }
-        assertTrue { Gender.detectGender(femaleString) == Gender.FEMALE }
-        assertTrue { Gender.detectGender(wrongString) == Gender.ANDROGYNOUS }
+        Assertions.assertEquals(Gender.MALE, maleGender)
+        Assertions.assertEquals(Gender.FEMALE, femaleGender)
+        Assertions.assertEquals(Gender.ANDROGYNOUS, androgynousGender)
     }
 
-    @Test
-    fun firstNameTest() {
-        val p = Petrovich()
-        val strings = loadTestSources("FirstNames.csv")
-
-        for (line in strings) {
-            if (line.size == 4) {
-                val testName = line[0]
-                val genderCase = Gender.valueOf(line[1].toUpperCase())
-                val nameCase = Case.valueOf(line[2].toUpperCase())
-                val assertName = line[3]
-
-                val result = p.firstName(testName, genderCase, nameCase)
-                System.out.printf("name=%s\tsex=%s\tcase=%s\texpected=%s\tresult=%s\n",
-                        testName,
-                        genderCase,
-                        nameCase,
-                        assertName,
-                        result)
-                assert(assertName == result)
-            }
-        }
+    @DisplayName("First name declension")
+    @ParameterizedTest(name = "{index}: first name=''{0}''; gender=''{1}''; case=''{2}''; result=''{3}''")
+    @CsvFileSource(resources = arrayOf("/FirstNames.csv"))
+    fun firstNameTest(
+            firstName: String,
+            gender: Gender,
+            case: Case,
+            expected: String) {
+        val result = PETROVICH.firstName(firstName, gender, case)
+        Assertions.assertEquals(expected, result)
     }
 
-    @Test
-    fun lastNameTest() {
-        val p = Petrovich()
-        val strings = loadTestSources("LastNames.csv")
-
-        for (line in strings) {
-            if (line.size == 4) {
-                val testName = line[0]
-                val genderCase = Gender.valueOf(line[1].toUpperCase())
-                val nameCase = Case.valueOf(line[2].toUpperCase())
-                val assertName = line[3]
-
-                val result = p.lastName(testName, genderCase, nameCase)
-                System.out.printf("name=%s\tsex=%s\tcase=%s\tresult=%s\n", testName, genderCase, nameCase, result)
-                assert(assertName == result)
-            }
-        }
+    @DisplayName("Last name declension")
+    @ParameterizedTest(name = "{index}: last name=''{0}''; gender=''{1}''; case=''{2}''; result=''{3}''")
+    @CsvFileSource(resources = arrayOf("/LastNames.csv"))
+    fun lastNameTest(
+            lastName: String,
+            gender: Gender,
+            case: Case,
+            expected: String) {
+        val result = PETROVICH.lastName(lastName, gender, case)
+        Assertions.assertEquals(expected, result)
     }
 
-    @Test
-    fun middleNameTest() {
-        val p = Petrovich()
-        val strings = loadTestSources("MiddleNames.csv")
-
-        for (line in strings) {
-            if (line.size == 4) {
-                val testName = line[0]
-                val genderCase = Gender.valueOf(line[1].toUpperCase())
-                val nameCase = Case.valueOf(line[2].toUpperCase())
-                val assertName = line[3]
-
-                val result = p.middleName(testName, genderCase, nameCase)
-                val resultDetectGender = p.middleName(testName, nameCase)
-                System.out.printf("name=%s\tsex=%s\tcase=%s\texpected=%s\tresult=%s\n",
-                        testName,
-                        genderCase,
-                        nameCase,
-                        assertName,
-                        result)
-                assert(assertName == result)
-                assert(assertName == resultDetectGender)
-            }
-        }
-    }
-
-    private fun loadTestSources(path: String): Collection<List<String>> {
-        val res = ArrayList<List<String>>()
-        val classloader = Thread.currentThread().contextClassLoader
-
-        classloader.getResourceAsStream(path).use {
-            BufferedReader(InputStreamReader(it, "UTF-8")).use {
-                var str: String? = it.readLine()
-                while (str != null) {
-                    val line: List<String> = str.replace(" ", "").split(",")
-                    res.add(line)
-                    str = it.readLine()
-                }
-            }
-        }
-        return res
+    @DisplayName("Middle name declension")
+    @ParameterizedTest(name = "{index}: middle name=''{0}''; gender=''{1}''; case=''{2}''; result=''{3}''")
+    @CsvFileSource(resources = arrayOf("/MiddleNames.csv"))
+    fun middleNameTest(
+            middleName: String,
+            gender: Gender,
+            case: Case,
+            expected: String) {
+        val result = PETROVICH.middleName(middleName, gender, case)
+        Assertions.assertEquals(expected, result)
     }
 }
